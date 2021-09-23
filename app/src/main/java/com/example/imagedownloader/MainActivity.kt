fe5db2e5c58ef1b0e9715657f8acb62e3a724cec
Nhien Nguyen <annhien.se@gmail.com>
2021-09-23 15:26:58 +0700

    add lifecycle testing

--- app/src/main/java/com/example/imagedownloader/MainActivity.kt
+++ app/src/main/java/com/example/imagedownloader/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.imagedownloader
 
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,15 +10,18 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("lifecycle: ","OnCreate")
         val imgView: ImageView = findViewById<ImageView>(R.id.imgView)
         val edtURL:EditText = findViewById<EditText>(R.id.edtURL)
         val btnDownload = findViewById<Button>(R.id.btnDownload)
         val btnLottie = findViewById<Button>(R.id.btnLottie)
+        val btnAlert = findViewById<Button>(R.id.btnAlert)
         val thread = Thread.currentThread()
         Log.i("MainActivity: ","Main Activity is running on thread ${thread.id}")
         btnDownload.setOnClickListener {
@@ -33,6 +38,52 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LottieActivity::class.java)
             startActivity(intent)
         }
+        btnAlert.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
 
+            // set message of alert dialog
+            dialogBuilder.setMessage("Do you want to close this application ?")
+                // if the dialog is cancelable
+                .setCancelable(false)
+                // positive button text and action
+                .setPositiveButton("Proceed", DialogInterface.OnClickListener {
+                        dialog, id -> finish()
+                })
+                // negative button text and action
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+                        dialog, id -> dialog.cancel()
+                })
+            // create dialog box
+            val alert = dialogBuilder.create()
+            // set title for alert dialog box
+            alert.setTitle("AlertDialogExample")
+            // show alert dialog
+            alert.show()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("lifecycle: ","OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("lifecycle: ","OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("lifecycle: ","OnStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("lifecycle: ","OnRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("lifecycle: ","OnDestroy")
     }
 }
